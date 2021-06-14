89dd8a28e40ba13f75399e6cb2e4bed627067e3a
Jared Brown <jaredbrown1@hotmail.com>
2021-06-14 00:02:17 -0700

    jokes api hooked up and responding to requests

--- script.js
+++ script.js
@@ -105,17 +105,39 @@ const VoiceRSS = {
 	},
 };
 
-function test() {
-	VoiceRSS.speech({
-		key: "f130b13e475048c09217045094d918a7",
-		src: "Hello, world!",
-		hl: "en-us",
-		v: "Linda",
-		r: 0,
-		c: "mp3",
-		f: "44khz_16bit_stereo",
-		ssml: false,
-	});
+// function test() {
+// 	VoiceRSS.speech({
+// 		key: "f130b13e475048c09217045094d918a7",
+// 		src: "Hello, world!",
+// 		hl: "en-us",
+// 		v: "Linda",
+// 		r: 0,
+// 		c: "mp3",
+// 		f: "44khz_16bit_stereo",
+// 		ssml: false,
+// 	});
+// }
+
+// test();
+
+// Get Jokes from Joke API
+async function getJokes() {
+	let joke = "";
+	const apiUrl =
+		"https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,explicit";
+	try {
+		const response = await fetch(apiUrl);
+		const data = await response.json();
+		if (data.setup) {
+			joke = `${data.setup} ... ${data.delivery}`;
+		} else {
+			joke = data.joke;
+		}
+		console.log(joke);
+	} catch (error) {
+		// Catch Errors Here
+		console.log("whoops", error);
+	}
 }
 
-test();
+getJokes();
